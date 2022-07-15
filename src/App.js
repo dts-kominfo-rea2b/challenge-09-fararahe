84e3c43ff33ca994d8cf83664c635b009dffd8f2
fararahe <faradyna.rahma@gmail.com>
2022-07-15 23:13:43 +0700

    Pengenalan React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import contacts from './data/contacts.json';
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
@@ -10,7 +11,12 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <Contact />
+      <Contact data={contacts[0]} />
+      <Contact data={contacts[1]} />
+      <Contact data={contacts[2]} />
+      <Contact data={contacts[3]} />
+      <Contact data={contacts[4]} />
+      <Contact data={contacts[5]} />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
-import contacts from '../data/contacts.json';
 
-const Contact = () => {
+const Contact = ({data}) => {
+    const {photo, name, phone, email} = data;
     return (
-        <div>
-            {
-                contacts.map((item) => {
-                    return <div className='Contact'>
-                        <div className='Left'>
-                            <img src={item.photo} alt={item.name}/>
-                        </div>
-                        <div className='Right'>
-                            <p>
-                                <b>{item.name}</b> <br/>
-                                {item.phone} <br/>
-                                {item.email}
-                            </p>
-                        </div>
-                    </div>
-                })
-            }
+        <div className='Contact'>
+            <div className='Left'>
+                <img src={photo} alt={name}/>
+            </div>
+            <div className='Right'>
+                <p>
+                    <b>{name}</b> <br/>
+                    {phone} <br/>
+                    {email}
+                </p>
+            </div>
         </div>
     )
 }
